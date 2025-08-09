053470c8734e17084fc73580982d8c694255acbc
Vasudev <79050603+Velstruck@users.noreply.github.com>
2025-08-09 22:43:19 +0530

    Main section intitailized, navbar with upload button added

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Links } from 'react-router'
+import { Link } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <Link to="/" className='text-2xl font-bold text-gradient' >
+        <p>RESUME BOLTE</p>
+      </Link>
+      <Link to="/upload" className="primary-button w-fit">
+      Upload Resume
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
